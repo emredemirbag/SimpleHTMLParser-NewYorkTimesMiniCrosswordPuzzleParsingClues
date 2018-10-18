bc40249ab034622850b65572d44296b779cc16c2
Emre Demirbag <emredemirbag@users.noreply.github.com>
2018-10-18 09:45:01 +0300

    Update HTMLParseHTML.py

--- HTMLParseHTML.py
+++ HTMLParseHTML.py
@@ -1,5 +1,5 @@
 #Simple HTML Parser program by Emre Demirbag https://github.com/emredemirbag
-#This program sends HTTP request to New York Times Mini Crossword Puzzle,by Joel Fagliano,(https://www.nytimes.com/crosswords/game/mini) link and it parses clues from the page writes to html file.
+#New York Times Mini Crossword Puzzle,by Joel Fagliano,(https://www.nytimes.com/crosswords/game/mini) link and it parses clues from the page writes to html file.
 #Libraries: requests:(http://docs.python-requests.org/en/master/), soup:(https://www.crummy.com/software/BeautifulSoup/bs4/doc/) 
 
 
